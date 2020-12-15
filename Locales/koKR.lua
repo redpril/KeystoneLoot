6cdbacf3eea4ff697ad036ee42c5344e69ee0d0c
redpril <76026371+redpril@users.noreply.github.com>
2020-12-15 18:41:45 +0900

    Create koKR.lua

---
+++ Locales/koKR.lua
@@ -0,0 +1,16 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("KeystoneLoot", "koKR")
+
+if L then
+
+L["KeystoneLoot: Loaded"] = "KeystoneLoot: 로드"
+L["KeystoneLoot: Enabled"] = "KeystoneLoot: 활성화"
+L["KeystoneLoot: Disabled"] = "KeystoneLoot: 비활성화"
+L["KeystoneLoot does not have any function command."] = "KeystoneLoot에는 없는 기능 입니다."
+
+L["This shows the level of the item you'll find in this week's chest."] = "이번 주 상자에서 찾을 수있는 아이템의 레벨을 보여줍니다."
+L["Weekly Chest Reward"] = "주간 상자 보상"
+
+L["Loot Item Level: "] = "클리어 보상: "
+L["Weekly Chest Item Level: "] = "주간 보상 상자: "
+
+end
